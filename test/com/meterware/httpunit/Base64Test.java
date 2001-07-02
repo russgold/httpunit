102f07b1f1dba6633d3511d32b1b801a91a62cc6
Russell Gold <russ@gold-family.us>
2001-07-02 13:23:45 +0000

    Added test for length of base64 decoding string

--- test/com/meterware/httpunit/Base64Test.java
+++ test/com/meterware/httpunit/Base64Test.java
@@ -2,14 +2,14 @@ package com.meterware.httpunit;
 /********************************************************************************************************************
 * $Id$
 *
-* Copyright (c) 2000, Russell Gold
+* Copyright (c) 2000-2001, Russell Gold
 *
-* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-* documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+* documentation files (the "Software"), to deal in the Software without restriction, including without limitation
 * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
 * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 *
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions
 * of the Software.
 *
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
@@ -26,14 +26,17 @@ import junit.framework.TestSuite;
 
 /**
  * Tests the base64 converter.
+ *
+ * @author <a href="mailto:russgold@acm.org">Russell Gold</a>
+ * @author <a href="mailto:mtarruella@silacom.com">Marcos Tarruella</a> 
  **/
 public class Base64Test extends TestCase {
 
     public static void main(String args[]) {
         junit.textui.TestRunner.run( suite() );
     }
-    
-    
+
+
     public static Test suite() {
         return new TestSuite( Base64Test.class );
     }
@@ -43,7 +46,7 @@ public class Base64Test extends TestCase {
         super( name );
     }
 
-	
+
     public void testEncode() {
         assertEquals( "Result of encoding", "QWxhZGRpbjpvcGVuIHNlc2FtZQ==", Base64.encode( "Aladdin:open sesame" ) );
         assertEquals( "Result of encoding", "QWRtaW46Zm9vYmFy",             Base64.encode( "Admin:foobar" ) );
@@ -53,6 +56,17 @@ public class Base64Test extends TestCase {
     public void testDecode() {
         assertEquals( "Result of decoding", "Aladdin:open sesame", Base64.decode( "QWxhZGRpbjpvcGVuIHNlc2FtZQ==" ) );
         assertEquals( "Result of decoding", "Admin:foobar",        Base64.decode( "QWRtaW46Zm9vYmFy" ) );
+    }
+
+
+    public void testExceptionDecoding() {
+        try {
+            Base64.decode( "123");
+            fail("valid Base64 codes have a multiple of 4 characters");
+        } catch (Exception e) {
+            assertEquals("valid Base64 codes have a multiple of 4 characters",
+                e.getMessage());
+        }
     }
 
 }
