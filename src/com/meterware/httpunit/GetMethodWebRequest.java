6fe95bc9856f2bfd3fe0de858ce6c2e26463bdfc
Russell Gold <russ@gold-family.us>
2002-10-31 00:25:45 +0000

    Added support for HEAD requests

--- src/com/meterware/httpunit/GetMethodWebRequest.java
+++ src/com/meterware/httpunit/GetMethodWebRequest.java
@@ -20,12 +20,11 @@ package com.meterware.httpunit;
 *
 *******************************************************************************************************************/
 import java.net.URL;
-import java.io.IOException;
 
 /**
  * An HTTP request using the GET method.
  **/
-public class GetMethodWebRequest extends WebRequest {
+public class GetMethodWebRequest extends HeaderOnlyWebRequest {
 
 
     /**
@@ -57,21 +56,6 @@ public class GetMethodWebRequest extends WebRequest {
      **/
     public String getMethod() {
         return "GET";
-    }
-
-
-    /**
-     * Returns the query string defined for this request.
-     **/
-    public String getQueryString() {
-        try {
-            URLEncodedString encoder = new URLEncodedString();
-            getParameterHolder().recordPredefinedParameters( encoder );
-            getParameterHolder().recordParameters( encoder );
-            return encoder.getString();
-        } catch (IOException e) {
-            throw new RuntimeException( "Programming error: " + e );   // should never happen
-        }
     }
 
 
