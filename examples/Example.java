1c29df137ec53d337bab8a41137a0bf06a8754a1
Russell Gold <russ@gold-family.us>
2002-11-25 13:52:45 +0000

    Updated Example.java to point to always-on page

--- examples/Example.java
+++ examples/Example.java
@@ -1,29 +1,24 @@
 import com.meterware.httpunit.*;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-
-import org.xml.sax.*;
-
+/** This is a simple example of using HttpUnit to read and understand web pages. **/
 public class Example {
 
 
     public static void main( String[] params ) {
         try {
-            WebRequest          request;
-            WebResponse         response;
-            WebConversation     conversation = new WebConversation();
-            
-            request = new GetMethodWebRequest( "http://www.meterware.com/servlet/TopSecret" );
-            response = conversation.getResponse( request );
-            System.out.println( response );
+            // create the conversation object which will maintain state for us
+            WebConversation wc = new WebConversation();
 
-            WebForm loginForm = response.getForms()[0];
+            // Obtain the main page on the meterware web site
+            WebRequest request = new GetMethodWebRequest( "http://www.meterware.com" );
+            WebResponse response = wc.getResponse( request );
 
-            request = loginForm.getRequest();
-            request.setParameter( "name", "master" );
-            response = conversation.getResponse( request );
-            System.out.println( response.getText() );
+            // find the link which contains the string "HttpUnit" and click it
+            WebLink httpunitLink = response.getFirstMatchingLink( WebLink.MATCH_CONTAINED_TEXT, "HttpUnit" );
+            response = httpunitLink.click();
+
+            // print out the number of links on the HttpUnit main page
+            System.out.println( "The HttpUnit main page contains " + response.getLinks().length + " links" );
 
         } catch (Exception e) {
             System.err.println( "Exception: " + e );
