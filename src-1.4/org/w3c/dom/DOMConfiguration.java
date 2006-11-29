f928e0f721a75f3c1fb6c1f07b5fedb8d2e4a8df
Russell Gold <russ@gold-family.us>
2006-11-29 16:29:55 +0000

    compile under jdk 1.4 or jdk 1.5

---
+++ src-1.4/org/w3c/dom/DOMConfiguration.java
@@ -0,0 +1,16 @@
+package org.w3c.dom;
+/********************************************************************************************************************
+ * $Id$
+ *
+ * Copyright (c) 2006, Russell Gold
+ *
+ *******************************************************************************************************************/
+
+/**
+ * A dummy interface to allow HttpUnit to compile under JDK 1.4
+ *
+ * @author <a href="mailto:russgold@httpunit.org">Russell Gold</a>
+ */
+public interface DOMConfiguration {
+
+}
