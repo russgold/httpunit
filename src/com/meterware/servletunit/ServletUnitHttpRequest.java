a61170bcd5b9a38c5f759c4081d598939529a16d
Wolfgang Fahl <wf@bitplan.com>
2007-12-21 13:23:51 +0000

    CVS headers don't work in Subversion

--- src/com/meterware/servletunit/ServletUnitHttpRequest.java
+++ src/com/meterware/servletunit/ServletUnitHttpRequest.java
@@ -17,7 +17,6 @@ package com.meterware.servletunit;
 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
 * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 * DEALINGS IN THE SOFTWARE.
-* $Log$
 *
 *******************************************************************************************************************/
 import com.meterware.httpunit.WebRequest;
