9d59139df0dedcf0064a390c7478f1b521f15d9d
Wolfgang Fahl <wf@bitplan.com>
2008-03-30 12:25:35 +0000

    copyright year changed

--- test/com/meterware/httpunit/parsing/ParserPropertiesTest.java
+++ test/com/meterware/httpunit/parsing/ParserPropertiesTest.java
@@ -2,7 +2,7 @@ package com.meterware.httpunit.parsing;
 /********************************************************************************************************************
  * $Id$
  *
- * Copyright (c) 2002, Russell Gold
+ * Copyright (c) 2002-2008, Russell Gold
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
