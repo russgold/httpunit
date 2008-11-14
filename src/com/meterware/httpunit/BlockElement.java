4f561b85eb1592fd24b9707eb36385dadb614f1a
Wolfgang Fahl <wf@bitplan.com>
2008-11-14 22:28:36 +0000

    BR [ 1963211 ] FindBug: BlockElement not checking null by Matthew O. Smith

--- src/com/meterware/httpunit/BlockElement.java
+++ src/com/meterware/httpunit/BlockElement.java
@@ -1,4 +1,5 @@
 package com.meterware.httpunit;
+
 /********************************************************************************************************************
  * $Id$
  *
@@ -27,193 +28,196 @@ import com.meterware.httpunit.scripting.ScriptableDelegate;
 import com.meterware.httpunit.scripting.ScriptingHandler;
 
 /**
- * Represents a block-level element such as a paragraph or table cell, which can contain other elements.
- *
+ * Represents a block-level element such as a paragraph or table cell, which can
+ * contain other elements.
+ * 
  * @author <a href="mailto:russgold@httpunit.org">Russell Gold</a>
- *
+ * 
  * @since 1.6
- **/
-abstract public class BlockElement extends ParsedHTML implements HTMLSegment, HTMLElement {
+ */
+abstract public class BlockElement extends ParsedHTML implements HTMLSegment,
+		HTMLElement {
 
+	private ScriptingHandler	_scriptable;
+	private Node							_node;
 
-    private ScriptingHandler _scriptable;
-    private Node             _node;
+	/**
+	 * Returns the text value of this block.
+	 */
+	public String getText() {
+		if (_node == null) {
+			return "";
+		} else if (_node.getNodeType() == Node.TEXT_NODE) {
+			return _node.getNodeValue().trim();
+		} else if (!_node.hasChildNodes()) {
+			return "";
+		} else {
+			return NodeUtils.asText(_node.getChildNodes()).trim();
+		}
+	}
 
+	/**
+	 * Returns the tag for this block.
+	 */
+	public String getTagName() {
+		return _node == null ? "p" : _node.getNodeName();
+	}
 
-    /**
-     * Returns the text value of this block.
-     **/
-    public String getText() {
-        if (_node.getNodeType() == Node.TEXT_NODE) {
-            return _node.getNodeValue().trim();
-        } else if (_node == null || !_node.hasChildNodes()) {
-            return "";
-        } else {
-            return NodeUtils.asText( _node.getChildNodes() ).trim();
-        }
-    }
+	/**
+	 * Returns a copy of the domain object model associated with this HTML
+	 * segment.
+	 */
+	public Node getDOM() {
+		return super.getDOM();
+	}
 
+	// -------------------------------- HTMLElement methods
+	// ---------------------------------------
 
-    /**
-     * Returns the tag for this block.
-     */
-    public String getTagName() {
-        return _node == null ? "p" : _node.getNodeName();
-    }
+	/**
+	 * Returns the ID associated with this element. IDs are unique throughout the
+	 * HTML document.
+	 */
+	public String getID() {
+		return getAttribute("id");
+	}
 
+	/**
+	 * Returns the class attribute associated with this element.
+	 */
+	public String getClassName() {
+		return getAttribute("class");
+	}
 
-    /**
-     * Returns a copy of the domain object model associated with this HTML segment.
-     **/
-    public Node getDOM() {
-        return super.getDOM();
-    }
+	/**
+	 * Returns the name associated with this element.
+	 */
+	public String getName() {
+		return getAttribute("name");
+	}
 
+	/**
+	 * Returns the title associated with this element.
+	 */
+	public String getTitle() {
+		return getAttribute("title");
+	}
 
-//-------------------------------- HTMLElement methods ---------------------------------------
+	/**
+	 * Returns the delegate which supports scripting this element.
+	 */
+	public ScriptingHandler getScriptingHandler() {
+		if (_scriptable == null) {
+			_scriptable = HttpUnitOptions.getScriptingEngine().createHandler(this);
+		}
+		return _scriptable;
+	}
 
+	/**
+	 * handle the event that has the given script attached by compiling the
+	 * eventScript as a function and executing it
+	 * 
+	 * @param eventScript -
+	 *          the script to use
+	 * @deprecated since 1.7 - use doEventScript instead
+	 */
+	public boolean doEvent(String eventScript) {
+		return doEventScript(eventScript);
+	}
 
-    /**
-     * Returns the ID associated with this element. IDs are unique throughout the HTML document.
-     **/
-    public String getID() {
-        return getAttribute( "id" );
-    }
+	/**
+	 * optional do the event if it's defined
+	 * 
+	 * @param eventScript -
+	 *          the script to work on
+	 * @return true if the event script was handled
+	 */
+	public boolean doEventScript(String eventScript) {
+		return this.getScriptingHandler().doEventScript(eventScript);
+	}
 
+	public boolean handleEvent(String eventName) {
+		return this.getScriptingHandler().handleEvent(eventName);
+	}
 
-    /**
-     * Returns the class attribute associated with this element.
-     */
-    public String getClassName() {
-        return getAttribute( "class" );
-    }
+	public ScriptableDelegate getParentDelegate() {
+		return getResponse().getDocumentScriptable();
+	}
 
+	public ScriptableDelegate newScriptable() {
+		return new HTMLElementScriptable(this);
+	}
 
-    /**
-     * Returns the name associated with this element.
-     **/
-    public String getName() {
-        return getAttribute( "name" );
-    }
+	/**
+	 * get the attribute with the given name
+	 * 
+	 * @param name -
+	 *          the name of the attribute to get
+	 */
+	public String getAttribute(final String name) {
+		return NodeUtils.getNodeAttribute(_node, name);
+	}
 
+	/**
+	 * set the attribute with the given name to the given value
+	 * 
+	 * @param name -
+	 *          the name of the attribute to set
+	 * @param value -
+	 *          the value to use
+	 */
+	public void setAttribute(final String name, final Object value) {
+		NodeUtils.setNodeAttribute(_node, name, (value == null) ? null : value
+				.toString());
+	}
 
-    /**
-     * Returns the title associated with this element.
-     **/
-    public String getTitle() {
-        return getAttribute( "title" );
-    }
+	/**
+	 * remove the attribute with the given name
+	 * 
+	 * @param name -
+	 *          the name of the attribute
+	 */
+	public void removeAttribute(final String name) {
+		NodeUtils.removeNodeAttribute(_node, name);
+	}
 
+	/**
+	 * Returns true if this element may have an attribute with the specified name.
+	 */
+	public boolean isSupportedAttribute(String name) {
+		return false;
+	}
 
-    /**
-     * Returns the delegate which supports scripting this element.
-     */
-    public ScriptingHandler getScriptingHandler() {
-        if (_scriptable == null) {
-            _scriptable = HttpUnitOptions.getScriptingEngine().createHandler( this );
-        }
-        return _scriptable;
-    }
-    
-    /**
-     * handle the event that has the given script attached
-     * by compiling the eventScript as a function and  executing it
-     * @param eventScript - the script to use
-     * @deprecated since 1.7 - use doEventScript instead
-     */
-    public boolean doEvent( String eventScript ) {
-    	return doEventScript(eventScript);
-    }
-    
-    /**
-     * optional do the event if it's defined
-     * @param eventScript - the script to work on
-     * @return true if the event script was handled
-     */
-    public boolean doEventScript(String eventScript) {
-    	return this.getScriptingHandler().doEventScript(eventScript);
-    }
-    
+	public Node getNode() {
+		return _node;
+	}
 
-    public boolean handleEvent(String eventName) {
-    	return this.getScriptingHandler().handleEvent(eventName);
-    }
+	// ----------------------------------------------- Object methods
+	// -------------------------------------------------------
 
-    public ScriptableDelegate getParentDelegate() {
-        return getResponse().getDocumentScriptable();
-    }
+	public boolean equals(Object obj) {
+		return getClass().equals(obj.getClass()) && equals((BlockElement) obj);
+	}
 
+	private boolean equals(BlockElement block) {
+		return _node.equals(block._node);
+	}
 
-    public ScriptableDelegate newScriptable() {
-        return new HTMLElementScriptable( this );
-    }
+	public int hashCode() {
+		return _node.hashCode();
+	}
 
+	// ------------------------------------- protected members
+	// --------------------------------------------------------------
 
-    /**
-     * get the attribute with the given name
-     * @param name - the name of the attribute to get
-     */
-    public String getAttribute( final String name ) {
-        return NodeUtils.getNodeAttribute( _node, name );
-    }
+	protected BlockElement(WebResponse response, FrameSelector frame,
+			URL baseURL, String baseTarget, Node rootNode, String characterSet) {
+		super(response, frame, baseURL, baseTarget, rootNode, characterSet);
+		_node = rootNode;
+	}
 
-    /**
-     * set the attribute with the given name to the given value
-     * @param name - the name of the attribute to set
-     * @param value - the value to use
-     */
-    public void setAttribute( final String name, final Object value ) {
-    	NodeUtils.setNodeAttribute( _node, name,  (value == null) ? null : value.toString() );
-    }
-
-    /**
-     * remove the attribute with the given name
-     * @param name - the name of the attribute
-     */
-    public void removeAttribute( final String name ) {
-    	NodeUtils.removeNodeAttribute( _node, name );
-    }    
-
-    /**
-     * Returns true if this element may have an attribute with the specified name.
-     */
-    public boolean isSupportedAttribute( String name ) {
-        return false;
-    }
-
-
-    public Node getNode() {
-        return _node;
-    }
-
-//----------------------------------------------- Object methods -------------------------------------------------------
-
-
-    public boolean equals( Object obj ) {
-        return getClass().equals( obj.getClass() ) && equals( (BlockElement) obj );
-    }
-
-
-    private boolean equals( BlockElement block ) {
-        return _node.equals( block._node );
-    }
-
-
-    public int hashCode() {
-        return _node.hashCode();
-    }
-
-
-//------------------------------------- protected members --------------------------------------------------------------
-
-
-    protected BlockElement( WebResponse response, FrameSelector frame, URL baseURL, String baseTarget, Node rootNode, String characterSet ) {
-        super( response, frame, baseURL, baseTarget, rootNode, characterSet );
-        _node = rootNode;
-    }
-
-
-    protected int getAttributeValue( Node node, String attributeName, int defaultValue ) {
-        return NodeUtils.getAttributeValue( node, attributeName, defaultValue );
-    }
+	protected int getAttributeValue(Node node, String attributeName,
+			int defaultValue) {
+		return NodeUtils.getAttributeValue(node, attributeName, defaultValue);
+	}
 }
--- src/com/meterware/httpunit/HTMLElementBase.java
+++ src/com/meterware/httpunit/HTMLElementBase.java
@@ -1,4 +1,5 @@
 package com.meterware.httpunit;
+
 /********************************************************************************************************************
  * $Id$
  *
@@ -28,161 +29,156 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
 
-
 /**
- *
+ * 
  * @since 1.5.2
  * @author <a href="mailto:russgold@httpunit.org">Russell Gold</a>
- **/ 
-abstract
-class HTMLElementBase implements HTMLElement {
+ */
+abstract class HTMLElementBase implements HTMLElement {
 
-    private Node        _node;
-    private ScriptingHandler _scriptable;
-    private Set _supportedAttributes = new HashSet();
+	private Node							_node;
+	private ScriptingHandler	_scriptable;
+	private Set								_supportedAttributes	= new HashSet();
 
+	public String getID() {
+		return getAttribute("id");
+	}
 
-    public String getID() {
-        return getAttribute( "id" );
-    }
+	public String getClassName() {
+		return getAttribute("class");
+	}
 
+	public String getTitle() {
+		return getAttribute("title");
+	}
 
-    public String getClassName() {
-        return getAttribute( "class" );
-    }
+	public String getName() {
+		return getAttribute("name");
+	}
 
+	/**
+	 * Returns a scriptable object which can act as a proxy for this control.
+	 */
+	public ScriptingHandler getScriptingHandler() {
+		if (_scriptable == null) {
+			_scriptable = HttpUnitOptions.getScriptingEngine().createHandler(this);
+		}
+		return _scriptable;
+	}
 
-    public String getTitle() {
-        return getAttribute( "title" );
-    }
+	/**
+	 * handle the event that has the given script attached by compiling the
+	 * eventScript as a function and executing it
+	 * 
+	 * @param eventScript -
+	 *          the script to use
+	 * @deprecated since 1.7 - use doEventScript instead
+	 */
+	public boolean doEvent(String eventScript) {
+		return doEventScript(eventScript);
+	}
 
+	/**
+	 * optional do the event if it's defined
+	 */
+	public boolean doEventScript(String eventScript) {
+		return this.getScriptingHandler().doEventScript(eventScript);
+	}
 
-    public String getName() {
-        return getAttribute( "name" );
-    }
+	public boolean handleEvent(String eventName) {
+		return this.getScriptingHandler().handleEvent(eventName);
+	}
 
+	/**
+	 * Returns the text value of this block.
+	 */
+	public String getText() {
+		if (_node == null) {
+			return "";
+		} else if (_node.getNodeType() == Node.TEXT_NODE) {
+			return _node.getNodeValue().trim();
+		} else if (!_node.hasChildNodes()) {
+			return "";
+		} else {
+			return NodeUtils.asText(_node.getChildNodes()).trim();
+		}
+	}
 
-    /**
-     * Returns a scriptable object which can act as a proxy for this control.
-     */
-    public ScriptingHandler getScriptingHandler() {
-        if (_scriptable == null) {
-            _scriptable = HttpUnitOptions.getScriptingEngine().createHandler( this );
-        }
-        return _scriptable;
-    }
-    
-    /**
-     * handle the event that has the given script attached
-     * by compiling the eventScript as a function and  executing it
-     * @param eventScript - the script to use
-     * @deprecated since 1.7 - use doEventScript instead
-     */
-    public boolean doEvent( String eventScript ) {
-    	return doEventScript(eventScript);
-    }
-    
-    /**
-     * optional do the event if it's defined
-     */
-    public boolean doEventScript(String eventScript) {
-    	return this.getScriptingHandler().doEventScript(eventScript);
-    }
-    
+	public String getTagName() {
+		return _node.getNodeName();
+	}
 
-    public boolean handleEvent(String eventName) {
-    	return this.getScriptingHandler().handleEvent(eventName);
-    }
-   
+	/**
+	 * construct me from a node
+	 * 
+	 * @param node -
+	 *          the node to get me from
+	 */
+	protected HTMLElementBase(Node node) {
+		_node = node;
+		// default attributes every html element can have
+		supportAttribute("id");
+		supportAttribute("class");
+		supportAttribute("title");
+		supportAttribute("name");
+	}
 
-    /**
-     * Returns the text value of this block.
-     **/
-    public String getText() {
-        if (_node.getNodeType() == Node.TEXT_NODE) {
-            return _node.getNodeValue().trim();
-        } else if (_node == null || !_node.hasChildNodes()) {
-            return "";
-        } else {
-            return NodeUtils.asText( _node.getChildNodes() ).trim();
-        }
-    }
+	/**
+	 * get the Attribute with the given name - by delegating to NodeUtils
+	 * 
+	 * @param name -
+	 *          the name of the attribute to get
+	 * @return the attribute
+	 */
+	public String getAttribute(final String name) {
+		return NodeUtils.getNodeAttribute(getNode(), name);
+	}
 
+	/**
+	 * set the Attribute with the given name - by delegating to NodeUtils
+	 * 
+	 * @param name -
+	 *          the name of the attribute to set
+	 * @param value -
+	 *          the value to set
+	 */
+	public void setAttribute(final String name, final Object value) {
+		NodeUtils.setNodeAttribute(getNode(), name, (value == null) ? null : value
+				.toString());
+	}
 
-    public String getTagName() {
-        return _node.getNodeName();
-    }
+	/**
+	 * remove the Attribute with the given name - by delegating to NodeUtils
+	 * 
+	 * @param name -
+	 *          the name of the attribute to remove
+	 */
+	public void removeAttribute(final String name) {
+		NodeUtils.removeNodeAttribute(getNode(), name);
+	}
 
+	public boolean isSupportedAttribute(String name) {
+		return _supportedAttributes.contains(name);
+	}
 
-    /**
-     * construct me from a node
-     * @param node - the node to get me from
-     */
-    protected HTMLElementBase( Node node ) {
-        _node = node;
-        // default attributes every html element can have
-        supportAttribute( "id" );
-        supportAttribute( "class" );
-        supportAttribute( "title" );
-        supportAttribute( "name" );
-    }
+	protected String getAttribute(final String name, String defaultValue) {
+		return NodeUtils.getNodeAttribute(getNode(), name, defaultValue);
+	}
 
+	public Node getNode() {
+		return _node;
+	}
 
-    /**
-     * get the Attribute with the given name - by delegating to
-     * NodeUtils
-     * @param name - the name of the attribute to get
-     * @return the attribute
-     */
-    public String getAttribute( final String name ) {
-        return NodeUtils.getNodeAttribute( getNode(), name );
-    }
-    
-    /**
-     * set the Attribute with the given name - by delegating to NodeUtils
-     * @param name - the name of the attribute to set
-     * @param value - the value to set
-     */
-    public void setAttribute( final String name, final Object value ) {
-       NodeUtils.setNodeAttribute( getNode(), name,  (value == null) ? null : value.toString() );
-    }
-         
-    /**
-     * remove the Attribute with the given name - by delegating to NodeUtils
-     * @param name - the name of the attribute to remove
-     */   
-    public void removeAttribute( final String name ) {
-       NodeUtils.removeNodeAttribute( getNode(), name );
-    }    
+	protected void supportAttribute(String name) {
+		_supportedAttributes.add(name);
+	}
 
-
-    public boolean isSupportedAttribute( String name ) {
-        return _supportedAttributes.contains( name );
-    }
-
-
-    protected String getAttribute( final String name, String defaultValue ) {
-        return NodeUtils.getNodeAttribute( getNode(), name, defaultValue );
-    }
-
-
-    public Node getNode() {
-        return _node;
-    }
-
-
-    protected void supportAttribute( String name ) {
-        _supportedAttributes.add( name );
-    }
-
-
-    /**
-     * Creates and returns a scriptable object for this control. Subclasses should override this if they use a different
-     * implementation of Scriptable.
-     */
-    public ScriptableDelegate newScriptable() {
-        return new HTMLElementScriptable( this );
-    }
-
+	/**
+	 * Creates and returns a scriptable object for this control. Subclasses should
+	 * override this if they use a different implementation of Scriptable.
+	 */
+	public ScriptableDelegate newScriptable() {
+		return new HTMLElementScriptable(this);
+	}
 
 }
