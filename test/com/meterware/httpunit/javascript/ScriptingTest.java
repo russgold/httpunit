13e9220f961411f2f649f0ed475c271792e9ed80
Wolfgang Fahl <wf@bitplan.com>
2009-08-17 12:05:41 +0000

    Header added

--- test/com/meterware/httpunit/javascript/ScriptingTest.java
+++ test/com/meterware/httpunit/javascript/ScriptingTest.java
@@ -1,8 +1,9 @@
 package com.meterware.httpunit.javascript;
 /********************************************************************************************************************
  * $Id$
+ * $URL$
  *
- * Copyright (c) 2002-2008, Russell Gold
+ * Copyright (c) 2002-2009, Russell Gold
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
