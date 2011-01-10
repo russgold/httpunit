0e14fe8b94099b0399a5a65692c347b8a1b0a7ce
Wolfgang Fahl <wf@bitplan.com>
2011-01-10 13:56:24 +0000

    fixed according to changes on simple.wiktionary.org

--- examples/NearWords.java
+++ examples/NearWords.java
@@ -8,8 +8,9 @@ import org.xml.sax.SAXException;
 
 /**
  * This class demonstrates using httpunit to use the functionality of a web set
- * from a command line. To use it, specify a single word to be searched.
- * The program will use the Wiktionary web site to find all links which match the pattern.
+ * from a command line. To use it, specify a single word to be searched. The
+ * program will use the Wiktionary web site to find all links which match the
+ * pattern.
  * 
  * Note: this program is not robust, but should work if used properly.
  **/
@@ -17,6 +18,7 @@ public class NearWords {
 
 	/***
 	 * start a search for the given word
+	 * 
 	 * @param params
 	 */
 	public static void main(String[] params) {
@@ -43,18 +45,18 @@ public class NearWords {
 }
 
 /**
- * subclass to seek words from the Wiktionary as of 2009-08-21
+ * subclass to seek words from the Wiktionary as of 2010-01-10
  * 
  */
 class WordSeeker {
-
+	String url = "http://simple.wiktionary.org/";
+	
 	/**
 	 * create a word seeker that visits wiktionary.org
 	 */
 	public WordSeeker() {
 		try {
 			HttpUnitOptions.setScriptingEnabled(false);
-			String url = "http://simple.wiktionary.org/";
 			System.out.println("visiting " + url);
 			WebRequest request = new GetMethodWebRequest(url);
 			response = conversation.getResponse(request);
@@ -73,43 +75,53 @@ class WordSeeker {
 	public String[] getWordsMatching(String pattern) throws Exception {
 		System.out.println("getting Words matching '" + pattern + "'");
 		WebForm lookupForm = response.getFormWithID("searchform");
-		SubmitButton fulltextButton = lookupForm.getSubmitButton("fulltext");
+		SubmitButton fulltextButton = lookupForm.getSubmitButton("button");
+		if (fulltextButton==null){
+			System.err.println("Could not find the fulltext submit button on webpage "+url);
+			System.err.println("You might want to fix the source code and submit a patch to the httpunit Developer team");
+			String [] result={"No result"};
+			return result;
+		}
 		System.out.println("Clicking " + fulltextButton.getName());
 		WebRequest request = lookupForm.getRequest(fulltextButton);
 		request.setParameter("search", pattern);
 		response = lookupForm.submit(fulltextButton);
 		// alternative: goButton.click();
-		return getOptionsFromResponse();
+		return getOptionsFromResponse(pattern);
 	}
 
 	private WebConversation conversation = new WebConversation();
+
 	private WebResponse response;
 
 	/**
 	 * 
+	 * @param pattern 
 	 * @return
 	 * @throws Exception
 	 */
-	private String[] getOptionsFromResponse() throws Exception {
+	private String[] getOptionsFromResponse(String pattern) throws Exception {
 		String[] words;
 		BrowserDisplayer.showResponseInBrowser(response);
 		WebLink[] links = response.getLinks();
-		ArrayList result=new ArrayList();
+		ArrayList result = new ArrayList();
 		// start from the "Advanced" link section
-		boolean start=false;
+		boolean start = false;
 		for (int i = 0; i < links.length; i++) {
 			WebLink link = links[i];
-			if (link.getText().startsWith("Advanced"))
-				start=true;
-			else if (link.getText().startsWith("next 20"))
-				break;
-			else if (start && !link.getText().startsWith("(more"))
+			String linkText=link.getText();
+			//if (linkText.startsWith("Advanced"))
+			//	start = true;
+			//else if (link.getText().startsWith("next 20"))
+			//	break;
+			//else if (start && !link.getText().startsWith("(more"))
+			if (linkText.indexOf(pattern)>=0)
 				result.add(link);
 		}
 		words = new String[result.size()];
-		for (int i=0;i<result.size();i++) {
-			WebLink link=(WebLink) result.get(i);
-			words[i]=link.getText()+" => "+link.getURLString();
+		for (int i = 0; i < result.size(); i++) {
+			WebLink link = (WebLink) result.get(i);
+			words[i] = link.getText() + " => " + link.getURLString();
 		}
 		return words;
 	}
