88d5f2553d97274a20707f3774103abe97054869
Wolfgang Fahl <wf@bitplan.com>
2011-01-10 13:39:48 +0000

    improved documentation and changed proxy settings to constants. Added System.out message to explain how to modify this example

--- examples/ProxySample.java
+++ examples/ProxySample.java
@@ -32,16 +32,36 @@ import junit.framework.TestSuite;
 public class ProxySample extends TestCase {
 
 
+	// modify proxyURL and proxyPort to your personal preferences
+	// e.g. a proxy server that is accessible from your network
+	public static String proxyURL="www-proxy.us.oracle.com";
+	public static int proxyPort=80;
+	
+		/**
+		 * main routine to test 
+		 * @param args
+		 */
     public static void main(String args[]) {
+    		System.out.println("Will run this sample JUnit Testcase with proxy URL set to "+proxyURL+" port "+proxyPort);
+    		System.out.println("You might want to modify the proxy server URL (see proxyURL,proxyPort fields in ProxySample.java) to one that you may use at your location for quicker response time");
+    		// run this example as a Unit test
         junit.textui.TestRunner.run( suite() );
     }
 
 
+    /**
+     * create a Testsuite containing this ProxySample class
+     * @return the testsuite
+     */
     public static TestSuite suite() {
         return new TestSuite( ProxySample.class );
     }
 
 
+    /**
+     * constructor which just calls the super constructor
+     * @param name
+     */
     public ProxySample( String name ) {
         super( name );
     }
@@ -54,10 +74,11 @@ public class ProxySample extends TestCase {
      */
     public void testProxyAccess() throws Exception {
         WebConversation wc = new WebConversation();
-        wc.setProxyServer( "www-proxy.us.oracle.com", 80 );
+        wc.setProxyServer( proxyURL, proxyPort );
         wc.getResponse( "http://www.meterware.com" );
     }
 
 
 
 }
+
