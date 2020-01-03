d73169c1600810dd5f088554652da15ab3ee7316
Jeremy Landis <jeremylandis@hotmail.com>
2020-01-02 21:45:13 -0500

    [servlet] Add support for servlet 3+

--- src/main/java/com/meterware/servletunit/ServletUnitHttpRequest.java
+++ src/main/java/com/meterware/servletunit/ServletUnitHttpRequest.java
@@ -32,10 +32,17 @@ import java.net.MalformedURLException;
 import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.AsyncContext;
+import javax.servlet.DispatcherType;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 
@@ -893,6 +900,82 @@ class ServletUnitHttpRequest implements HttpServletRequest {
                                                 : (_priority < other._priority ? +1 : -1 );
         }
 
+    }
+
+
+    public ServletContext getServletContext() {
+      try {
+        return _servletRequest.getServlet().getServletConfig().getServletContext();
+      } catch (ServletException e) {
+        return null;
+      }
+    }
+
+
+    public AsyncContext startAsync() throws IllegalStateException {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+
+    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse)
+        throws IllegalStateException {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+
+    public boolean isAsyncStarted() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+
+    public boolean isAsyncSupported() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+
+    public AsyncContext getAsyncContext() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+
+    public DispatcherType getDispatcherType() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+
+    public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+
+    public void login(String username, String password) throws ServletException {
+      // TODO Auto-generated method stub
+      
+    }
+
+
+    public void logout() throws ServletException {
+      // TODO Auto-generated method stub
+      
+    }
+
+
+    public Collection<Part> getParts() throws IOException, ServletException {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+
+    public Part getPart(String name) throws IOException, ServletException {
+      // TODO Auto-generated method stub
+      return null;
     }
 }
 
